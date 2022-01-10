47fc83c7e1e9fc18be1be6407c0983c9eff86870
Ant1B2x <antoine.bedex@tutanota.com>
2022-01-10 15:01:20 +0100

    add activity finished

--- app/src/main/java/com/example/gallery/AddActivity.kt
+++ app/src/main/java/com/example/gallery/AddActivity.kt
@@ -8,10 +8,20 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gallery.databinding.ActivityAddBinding
 import com.github.dhaval2404.imagepicker.ImagePicker
+import android.graphics.Bitmap
+
+import android.graphics.drawable.BitmapDrawable
+import android.widget.ImageView
+import androidx.room.Room
+
+import java.io.ByteArrayOutputStream
+
 
 class AddActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddBinding
+    private lateinit var pictureDao: PictureDao
+    private var pictureSelected: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +32,19 @@ class AddActivity : AppCompatActivity() {
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "picturedatabase"
+        ).allowMainThreadQueries().enableMultiInstanceInvalidation()
+            .fallbackToDestructiveMigration().build()
+
+        pictureDao = db.pictureDao()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     fun pickImage(view: android.view.View) {
@@ -42,6 +65,8 @@ class AddActivity : AppCompatActivity() {
 
                 // Use Uri object instead of File to avoid storage permissions
                 binding.imageViewPicture.setImageURI(uri)
+
+                pictureSelected = true
             }
             ImagePicker.RESULT_ERROR -> {
                 Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
@@ -52,12 +77,33 @@ class AddActivity : AppCompatActivity() {
         }
     }
 
-    fun savePicture(view: android.view.View) {
-        onBackPressed()
+    fun imageViewToBytes(iv: ImageView): ByteArray {
+        val bitmap = (iv.getDrawable() as BitmapDrawable).bitmap
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
+        return stream.toByteArray()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+    fun savePicture(view: android.view.View) {
+        if (pictureSelected) {
+            println(binding.editTextTitle)
+            println(binding.editTextLocation)
+            println(binding.editTextDescription)
+
+            val blob = imageViewToBytes(binding.imageViewPicture)
+            val picture = Picture (
+                blob = blob,
+                title = binding.editTextTitle.text.toString(),
+                description = binding.editTextDescription.text.toString(),
+                location = binding.editTextLocation.text.toString(),
+                favorite = false
+            )
+            pictureDao.insertAll(picture)
+
+            onBackPressed()
+        } else {
+            Toast.makeText(this, "You have to select a picture first!", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
--- app/src/main/java/com/example/gallery/AppDatabase.kt
+++ app/src/main/java/com/example/gallery/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.gallery
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Picture::class), version = 1)
+@Database(entities = arrayOf(Picture::class), version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun pictureDao(): PictureDao
 }
