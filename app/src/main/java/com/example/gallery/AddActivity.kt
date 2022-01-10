05e5a4403bd6e3236d9e3620fb4f1dfa23e7f244
Ant1B2x <antoine.bedex@tutanota.com>
2022-01-10 11:33:06 +0100

    picture selector done

--- app/src/main/java/com/example/gallery/AddActivity.kt
+++ app/src/main/java/com/example/gallery/AddActivity.kt
@@ -1,9 +1,13 @@
 package com.example.gallery
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gallery.databinding.ActivityAddBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class AddActivity : AppCompatActivity() {
 
@@ -20,7 +24,35 @@ class AddActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayShowHomeEnabled(true)
     }
 
-    fun runMainActivity(view: android.view.View) {
+    fun pickImage(view: android.view.View) {
+        ImagePicker.with(this)
+            .crop()
+            .compress(1024)
+            .maxResultSize(1080, 1080)
+            .start()
+    }
+
+    // Handle when picture is selected
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                //Image Uri will not be null for RESULT_OK
+                val uri: Uri = data?.data!!
+
+                // Use Uri object instead of File to avoid storage permissions
+                binding.imageViewPicture.setImageURI(uri)
+            }
+            ImagePicker.RESULT_ERROR -> {
+                Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Toast.makeText(this, "Selection Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun savePicture(view: android.view.View) {
         onBackPressed()
     }
 
